bba76ad86c65e20862310d9d94cab701a9d2b367
Ben Little <ben.little@benlittle.dev>
2021-08-07 11:00:07 -0400

    rm unnecessary args

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ cmd="xargo build --target=aarch64-unknown-linux-musl $@"
 podman run -ti --rm \
 	--name rs \
 	-v ./:/mnt \
-	-v sysroot:/root/.xargo \
 	-w /mnt \
 	-e RUST_BACKTRACE=1 \
-	-e CC_aarch64_unknown_linux_musl=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc \
-	-e CXX_aarch64_unknown_linux_musl=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-g++ \
-	-e QEMU_LD_PREFIX=/opt/aarch64-linux-musl-cross \
-	-e PKG_CONFIG_PATH=/opt/aarch64-linux-musl-cross/lib \
-	-e PKG_CONFIG_ALLOW_CROSS=1 \
 	localhost/webkit-aarch64 $cmd
+	# -e CC_aarch64_unknown_linux_musl=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-gcc \
+	# -e CXX_aarch64_unknown_linux_musl=/opt/aarch64-linux-musl-cross/bin/aarch64-linux-musl-g++ \
+	# -e QEMU_LD_PREFIX=/opt/aarch64-linux-musl-cross \
+	# -e PKG_CONFIG_PATH=/opt/aarch64-linux-musl-cross/lib \
+	# -e PKG_CONFIG_ALLOW_CROSS=1 \
