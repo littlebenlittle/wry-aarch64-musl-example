20d78d63f07a68abeae1b0376cac98172e7ab277
Ben Little <ben.little@benlittle.dev>
2021-08-10 15:44:53 -0400

    compile to armhf

--- metabuild.sh
+++ metabuild.sh
@@ -2,13 +2,14 @@
 
 set -e
 
+arch=armhf
+
 ctr=`buildah from rustembedded/cross:aarch64-unknown-linux-musl-0.2.1`
 trap "buildah rm $ctr" EXIT
 
+buildah run $ctr dpkg --add-architecture $arch
 buildah run $ctr apt-get update
-buildah run $ctr dpkg --add-architecture i386
 buildah run $ctr apt-get install -y \
-	libwebkit2gtk-4.0-dev:i386 \
-	libgtksourceviewmm-3.0-dev:i386
-buildah commit $ctr cross-wry:webkit
-buildah commit $ctr cross-wry
+	libwebkit2gtk-4.0-dev:$arch \
+	libgtksourceview-3.0-dev:$arch
+buildah commit $ctr cross-wry:$arch
